5db43c27c17cb6af9bf8c1762d8725387ba9f30b
taehyun1234 <nth0310@naver.com>
2021-10-08 21:11:07 +0900

    Add DFS and BFS

--- Algorithm Study/Algorithm Study/DFS와 BFS(1260번).cpp
+++ Algorithm Study/Algorithm Study/DFS와 BFS(1260번).cpp
@@ -1,17 +1,79 @@
 #include <iostream>
+#include <queue>
+#include <cstring>
+
 using namespace std;
 
-void DFS(int v) 
-{
+const int MAX = 1000 + 1;
+int N, M, V;
+int adjacent[MAX][MAX];
+bool visited[MAX];
 
+queue<int> q;
+
+void DFS(int idx)
+{
+	cout << idx << " ";
+
+	for (int i = 0; i < N; i++)
+	{
+		if (adjacent[idx][i+1] && !visited[i+1])
+		{
+			visited[i+1] = true;
+			//인접한 노드에 대해서 또 다시 DFS
+			DFS(i+1);
+		}
+	}
 }
 
-void BFS(int v)
-{
 
+
+void BFS(int idx)
+{
+	q.push(idx);
+	visited[idx] = 1;
+
+	while (!q.empty())
+	{
+		idx = q.front();
+		q.pop();
+
+		cout << idx << " ";
+		//BFS는 해당 노드에 인접한 노드들을 모두 추가한 뒤 BFS 진행
+
+		for (int i = 0; i < N; i++)
+		{
+			if (adjacent[idx][i+1] && !visited[i+1])	
+			{	
+				visited[i+1] = true;	
+				q.push(i+1);	
+			}
+		}
+	}
 }
 
-int main()
+
+
+int main(void)
 {
+	cin >> N >> M >> V;
+
+	for (int i = 0; i < M; i++)
+	{
+		int from, to;
+		cin >> from >> to;
+		adjacent[from][to] = 1;
+		adjacent[to][from] = 1;
+	}
+
+	visited[V] = 1; //V에서 시작하므로
+	DFS(V);
+	cout << endl;
+
+	memset(visited, false, sizeof(visited));
+	BFS(V);
+	cout << endl;
+
 	return 0;
+
 }
