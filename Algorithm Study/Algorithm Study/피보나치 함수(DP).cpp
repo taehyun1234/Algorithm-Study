09be7367241e93d1f05e96adbea29ac29f5a1442
taehyun1234 <nth0310@naver.com>
2021-10-09 20:00:10 +0900

    Add pibonacci(DP)

---
+++ Algorithm Study/Algorithm Study/피보나치 함수(DP).cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int dp[41] = { 0,1,1 };
+
+int fibonacci(int n) {
+	if (n == 0 || n == 1)
+		return dp[n];
+	if (dp[n] == 0) {			
+		dp[n] = fibonacci(n - 1) + fibonacci(n - 2);
+	}
+	return dp[n];
+}
+
+// 0 1 1 2 3 5 8 13 21 34 55
+
+int main()
+{
+	int testCase = 0;
+	vector<int> input;
+
+	cin >> testCase;
+
+	for (int i = 0; i < testCase; i++)
+	{
+		int temp;
+		cin >> temp;
+		input.push_back(temp);
+	}
+
+	for (int i = 0; i < testCase; i++)
+	{
+		if (i != 0)
+			cout << fibonacci(input[i-1]) << " " << fibonacci(input[i])<<endl;
+		else
+			cout << "1 0"<<endl;
+	}
+
+	return 0;
+}
---
+++ Algorithm Study/Algorithm Study/피보나치 함수.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int dp[41];
+
+int fibonacci(int n) {
+	if (n == 0) {
+		return 0;
+	}
+	else if (n == 1) {
+		return 1;
+	}
+	else 
+	{
+		return dp[n] + dp[n - 1];
+	}
+}
+
+int main()
+{
+	int testCase = 0;
+	vector<int> input;
+	vector<pair<int, int>> output;
+
+	cin >> testCase;
+
+	for (int i = 0; i < testCase; i++)
+	{
+		int temp;
+		cin >> temp;
+		input.push_back(temp);
+	}
+
+	for (int i = 0; i < testCase; i++)
+	{
+		zero = 0;
+		one = 0;
+		fibonacci(input[i]);
+		cout << zero << " " << one<<endl;
+	}
+
+	return 0;
+}
