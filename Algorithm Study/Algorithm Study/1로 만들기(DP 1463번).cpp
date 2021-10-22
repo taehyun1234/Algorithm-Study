60dc80ca507c9ba0387c356b3e77bc103861184a
taehyun1234 <nth0310@naver.com>
2021-10-22 20:32:54 +0900

    Add DP(Not Solved)

---
+++ Algorithm Study/Algorithm Study/1로 만들기(DP 1463번).cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+#define MAX_COUNT 1000001
+
+using namespace std;
+
+int dp[MAX_COUNT];
+
+int min(int a, int b)
+{
+	return a > b ? b : a;
+}
+
+int main()
+{
+	int answer = 0;
+	dp[0] = 0;
+	dp[1] = 0;												// 아무것도 안함.
+
+	// dp[n] = dp[n/3] + 1;					// 3으로 나눠떨어지는 경우
+	// dp[n] = dp[n/2] + 1;					// 2로 나눠떨어지는 경우
+	// dp[n] = dp[n-1] + 1;					// 1을 빼는 경우
+	
+	for (int i = 2; i <= MAX_COUNT; i++)
+	{
+		dp[i] = dp[i - 1] + 1;
+		if (i % 3 == 0)
+		{
+			dp[i] = min(dp[i], dp[i / 3] + 1);
+		}
+		else if (i % 2 == 0)
+		{
+			dp[i] = min(dp[i], dp[i / 2] + 1);
+		}
+	}
+	cin >> answer;
+
+	cout << dp[answer];
+
+	return 0;
+}
